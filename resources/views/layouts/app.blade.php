4c34de1d7dce6a2e0912de6c1137be2002621d71
Rookye team <github@rookye.com.mx>
2018-10-29 22:40:50 -0600

    prueba blade
    Segunda prueba de blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,24 @@
     <title>Lista de usuarios</title>
 </head>
 <body>
-    @yield('navbar')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="usuario/create">Agregar usuario <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
     <div class="container">
         @yield('content')
     </div>
